59f748791037f46333d88792d09f57917c18c4a7
Aslam <aslamriosis@gmail.com>
2018-08-30 15:39:27 +0530

    Added support for template configuration files

--- pinstall.sh
+++ pinstall.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-echo "Enter shell script name"
-read file
+args=$#
+if [  $args -lt 1 ]; then
+  echo "Usage: pinstall <shell_file>"
+  exit;
+fi
+file=$1
 if [ ! -e $file ]; then
   echo "Shell script not found!"
   exit;
 fi
 symlink=${file/\.sh/}
+folder=".$symlink"
 sudo chmod +x $file
-sudo cp $file /usr/bin/
-if [ -e /usr/bin/$symlink ]; then
-  echo "Removing existing symlink"
-  sudo rm -r /usr/bin/$symlink
+if [ -d /usr/bin/$folder ]; then
+  echo "Removing existing $symlink"
+  sudo rm -fr /usr/bin/$folder/
+  sudo rm -fr /usr/bin/$symlink
 fi
-sudo ln -s /usr/bin/$file /usr/bin/$symlink
+sudo mkdir /usr/bin/$folder
+sudo cp $file /usr/bin/$folder
+if ls *.conf 1> /dev/null 2>&1; then
+  for conf in *.conf; do sudo cp $conf /usr/bin/$folder ;done
+fi
+sudo ln -s /usr/bin/$folder/$file /usr/bin/$symlink
 if [ $? -eq 0 ]; then
   echo "Installed $symlink :)"
 else
