e6a7ef61633dfc0ac47356d28497d4bf69b8d1de
Aslam <kv.aslam@gmail.com>
2018-09-02 21:16:40 +0530

    Added support for listing previously installed scripts

--- pinstall.sh
+++ pinstall.sh
@@ -1,20 +1,80 @@
 #!/bin/bash
-echo "Enter shell script name"
-read file
-if [ ! -e $file ]; then
-  echo "Shell script not found!"
+file_exists (){
+  file=$1
+  if [ ! -e $file ]; then
+    echo "Shell script not found!"
+    exit;
+  fi
+}
+
+install (){
+  file=$1
+  file_exists $file
+
+  symlink=${file/\.sh/}
+  folder=".$symlink"
+
+  remove $folder $symlink
+
+  sudo chmod +x $file
+  sudo mkdir /usr/bin/$folder
+  sudo cp $file /usr/bin/$folder
+  if ls *.conf 1> /dev/null 2>&1; then
+    echo "Installing configurations"
+    for conf in *.conf; do sudo cp $conf /usr/bin/$folder ;done
+  fi
+  sudo ln -s /usr/bin/$folder/$file /usr/bin/$symlink
+  if [ $? -eq 0 ]; then
+    echo "Installed $symlink :)"
+  else
+    echo "Installation of $symlink failed :("
+  fi
+}
+
+remove (){
+  folder=$1
+  symlink=$2
+  if [ -d /usr/bin/$folder ]; then
+    echo "Removing existing $symlink"
+    sudo rm -fr /usr/bin/$folder/
+    sudo rm -f /usr/bin/$symlink
+    return 0
+  fi
+  return 1
+}
+
+args=$#
+if [  $args -lt 1 ]; then
+  echo "Usage\n"
+  echo "Installation: pinstall <shell_file>"
+  echo "List Installed scripts: pinstall -l"
+  echo "Removal: pinstall -r <shell_file>"
   exit;
 fi
-symlink=${file/\.sh/}
-sudo chmod +x $file
-sudo cp $file /usr/bin/
-if [ -e /usr/bin/$symlink ]; then
-  echo "Removing existing symlink"
-  sudo rm -r /usr/bin/$symlink
+if [ "$1" == "-r" ]; then
+  file=$2
+  symlink=${file/\.sh/}
+  folder=".$symlink"
+  remove $folder $symlink
+  if [ $? -eq 1 ]; then
+    echo "$symlink does not exist!"
+    exit;
+  fi
+  echo "Uninstalled $symlink"
+  exit;
 fi
-sudo ln -s /usr/bin/$file /usr/bin/$symlink
-if [ $? -eq 0 ]; then
-  echo "Installed $symlink :)"
-else
-  echo "Installation of $symlink failed :("
+if [ "$1" == "-l" ]; then
+  list=`ls /usr/bin/.[A-z]* 2> /dev/null | sed 's/\.sh$//g'`
+  total=${#list}
+  if [ $total -lt 1 ]; then
+    echo "No Installed scripts found"
+    exit;
+  fi
+  echo "Installed scripts"
+  echo $list
+  exit;
+fi
+if [  $args -eq 1 ]; then
+  echo "Preparing Installation"
+  install $1
 fi
